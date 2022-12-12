a71c0cd2b74d325784a5ec0de1f8148b687325fe
M-Java86 <mpythondata@outlook.com>
2022-12-12 13:33:57 -0500

    added JavaScript demo

---
+++ JavaScript Demo/app.js
@@ -0,0 +1,21 @@
+//var x = 10;
+//x="10"
+//if(x==10) {
+//console.log('This works')
+//}else{
+//console.log('this works'); //Declaring a variable. decalaring x is 10-->
+//}
+
+//var weather = 'sunny';
+//if (weather === 'sunny') {
+//console.log("I'll go outside");
+//} else if (weather === 'rainy') {
+//console.log('do your homework');
+//} else {
+// console.log("I'll read");
+//}
+
+//
+//for (let i = 0; i < 5; i++) {
+//console.log(i);
+//}
