0df7bf319059dbad7542e0d68a2b3a96cae23b60
M-Java86 <mpythondata@outlook.com>
2023-02-09 22:22:49 -0500

    modified

---
+++ Udemy/JavaScript Demo/app.js
@@ -0,0 +1,36 @@
+//var x = 10;
+//x="10"
+//if(x==10) {
+//console.log('This works')
+//}else{
+//console.log('this works'); //Declaring a variable. decalaring x is 10-->
+//}
+
+//var weather = 'sunny';
+//if (weather === 'sunny') {
+//console.log("I'll go outside");
+//} else if (weather === 'rainy') {
+//console.log('do your homework');
+//} else {
+// console.log("I'll read");
+//}
+
+//
+//for (let i = 0; i < 5; i++) {
+//console.log(i);
+//}
+//var x = 10;
+//function example() {
+ // x = 5;
+  //console.log(x);
+//}
+//example();
+
+//console.log(x);
+
+//Nan-not a number
+//parameters
+function my func(name1){
+  console.log(name1 + "is the best!")
+}
+myfunc()
---
+++ Udemy/Node.js/app.js
@@ -0,0 +1,24 @@
+//console.log('Hello World');
+
+//const username = 'Max';
+
+//console.log(`Hi {$userName}!`); //Template literal
+
+//Node.js- in the docs you will find all the core modules Node.js is made up of. File system module.
+//const fs = require('fs'); //pulls in filesystem from node.js
+//fs.readFile('user-data.txt', (err, data) => {
+//if (err) {
+//  console.log(err);
+//return;
+//}
+//console.log(data.toString());
+//});
+//fs.writeFile('user-data.txt', 'username=Max', (err) => //{
+//if (err) {
+//console.log(err);
+//} else {
+//  console.log('Wrote to file!');
+// }
+///});
+
+///writefile-takes path to a file and then the we wanna write to data to that file
